51c6d78bec9b2b6142ba43e2b92aaa7196f21535
SanqDuonq <namsang0902s@gmail.com>
2024-12-13 11:18:00 +0700

    Update profile feature

--- backend/services/auth.services.ts
+++ backend/services/auth.services.ts
@@ -2,14 +2,15 @@ import { User } from "../models/user.model";
 import createErrors from 'http-errors';
 import otpServices from "./otp.services";
 import emailServices from "./email.services";
-import { PassThrough } from "nodemailer/lib/xoauth2";
+import cloudinary from "../utils/cloudinary";
 
 export interface IAuthService {
     signUp(data: {email:string,password:string,fullName:string,profilePicture:string}): Promise<void>,
-    signIn(data: {email:string,password:string}):Promise<void>,
+    signIn(data: {email:string,password:string}):Promise<{email:string,userId:string}>,
     verifyEmail(data: {email:string,OTP:string}):Promise<void>,
     forgotPassword(email:string): Promise<void>,
     resetPassword(data:{OTP:string,newPassword:string}):Promise<void>,
+    updateProfile(data:{userId:string,profilePicture:string}): Promise<void>
 }
 
 export class AuthService implements IAuthService {
@@ -32,7 +33,7 @@ export class AuthService implements IAuthService {
         await newUser.save();
     }
 
-    async signIn(data: { email: string; password: string; }): Promise<void> {
+    async signIn(data: { email: string; password: string; }): Promise<{email: string, userId:string}> {
         const body = data;
         const user = await User.findOne({email: body.email})
         if (!user) {
@@ -42,6 +43,7 @@ export class AuthService implements IAuthService {
         if (!isPassword){
             throw createErrors(400, 'Email or password is wrong!')
         }
+        return {email: user.email, userId: user._id.toString()}
     }
 
     async verifyEmail(data: { email: string; OTP: string; }): Promise<void> {
@@ -88,6 +90,19 @@ export class AuthService implements IAuthService {
         await user.verifyPassword(newPassword);
 
         await user.save();
+    }
+    async updateProfile(data:{userId:string,profilePicture: string}): Promise<void> {
+        const body = data;
+        if (!body.profilePicture){
+            throw createErrors(400, 'Profile picture is required')
+        }
+        const uploadRes = await cloudinary.uploader.upload(body.profilePicture, {
+            folder: 'profile-user',
+            allowed_formats: ['jpg', 'png', 'gif']
+        })
+        await User.findByIdAndUpdate(body.userId,{
+            profilePicture: uploadRes.secure_url
+        },{new: true})
     }
 }
 
